a685d97980700fa0b7ad2a30bb150e75b3b3d31f
Somesk278 <skim7420@gmail.com>
2019-08-21 23:41:35 -0400

    Added more to home

--- src/App.js
+++ src/App.js
@@ -9,33 +9,38 @@ class App extends Component {
     this.state = {
       route: 'home',
       component: null
-    }
+    };
   }
 
   onRouteChange = (route) => {
-    console.log(route);
+    // Change the route
     this.setState({ route });
-  } 
+  }
 
   render() {
     // Async rendering
     switch (this.state.route) {
       case 'home':
-        return <Home onRouteChange={this.onRouteChange} route={this.state.route} />
+        return <Home onRouteChange={this.onRouteChange} route={this.state.route} />;
+
       case 'about':
-        const AsyncAbout = AsyncComponent(() => import('./Components/about'))
-        return <AsyncAbout onRouteChange={this.onRouteChange} route={this.state.route} />
+        const AsyncAbout = AsyncComponent(() => import('./Components/about'));
+        return <AsyncAbout onRouteChange={this.onRouteChange} route={this.state.route} />;
+
       case 'projects':
-        const AsyncProjects = AsyncComponent(() => import('./Components/projects'))
-        return <AsyncProjects onRouteChange={this.onRouteChange} route={this.state.route} />
+        const AsyncProjects = AsyncComponent(() => import('./Components/projects'));
+        return <AsyncProjects onRouteChange={this.onRouteChange} route={this.state.route} />;
+
       case 'resume':
-        const AsyncResume = AsyncComponent(() => import('./Components/resume'))
-        return <AsyncResume onRouteChange={this.onRouteChange} route={this.state.route} />
+        const AsyncResume = AsyncComponent(() => import('./Components/resume'));
+        return <AsyncResume onRouteChange={this.onRouteChange} route={this.state.route} />;
+
       case 'contact':
-        const AsyncContact = AsyncComponent(() => import('./Components/contact'))
-        return <AsyncContact onRouteChange={this.onRouteChange} route={this.state.route} />
+        const AsyncContact = AsyncComponent(() => import('./Components/contact'));
+        return <AsyncContact onRouteChange={this.onRouteChange} route={this.state.route} />;
+
       default:
-        return null
+        return null;
     }
 
   }
--- src/Components/home.js
+++ src/Components/home.js
@@ -5,7 +5,55 @@ const Home = ({ onRouteChange, route }) => {
     return (
         <div>
             <Nav onRouteChange={onRouteChange} route={route}></Nav>
-            <p>Home</p>
+            <div class="row home animated fadeIn">
+                <div class="col col-sm-auto picture">
+                    <div id="profile">
+                        <img class="portrait" src={require("../Pics/me.jpg")} itemprop="image" alt="Avatar" />
+                    </div>
+                </div>
+            </div>
+
+            <div class="col col-sm-auto info">
+                <div id="profile">
+                    <h1>Maybe something will go here????</h1>>
+                </div>
+            </div>
+
+            <footer class="bg-dark">
+                <ul>
+                    <li>
+                        <a
+                            id="fb"
+                            href="https://www.facebook.com/solomon.kim.319"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <img class="social" src={require("../Pics/fb.png")} alt="Facebook" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            id="github"
+                            href="https://github.com/Lotrius"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <img class="social" src={require("../Pics/github.png")} alt="GitHub" />
+                        </a>
+                    </li>
+                    <li>
+                        <a
+                            id="linkedin"
+                            href="https://www.linkedin.com/in/solomon-kim/"
+                            rel="noopener noreferrer"
+                            target="_blank"
+                        >
+                            <img class="social" src={require("../Pics/linkedin.png")} alt="LinkedIn" />
+                        </a>
+                    </li>
+                </ul>
+                <h1 id="copyright">Do I actually need to copyright this page??</h1>
+            </footer>
         </div>
     )
 }
