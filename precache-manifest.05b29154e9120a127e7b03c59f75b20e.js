05e1ac3403fd972c4b76822c03df6956065400f0
Somesk278 <skim7420@gmail.com>
2019-12-28 16:50:43 -0500

    Updates

---
+++ precache-manifest.05b29154e9120a127e7b03c59f75b20e.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c79816e0f86a691a03480ba80d5c52",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ab9a7a82bae0fc932dee",
+    "url": "/static/css/4.075bacf3.chunk.css"
+  },
+  {
+    "revision": "7b38bbb15872bd2101fb",
+    "url": "/static/css/5.2562a0b8.chunk.css"
+  },
+  {
+    "revision": "8fa5376b1e578af9d00e",
+    "url": "/static/css/7.5bbd5449.chunk.css"
+  },
+  {
+    "revision": "05d99fec4472a6ca62a9",
+    "url": "/static/css/8.90244a48.chunk.css"
+  },
+  {
+    "revision": "73ceaa80faeaf10e1b1b",
+    "url": "/static/css/main.a20bed21.chunk.css"
+  },
+  {
+    "revision": "e997f3d7a3443a19d9bd",
+    "url": "/static/js/0.8dae08d8.chunk.js"
+  },
+  {
+    "revision": "2fd8b608a36e651c3dce",
+    "url": "/static/js/1.c10207fe.chunk.js"
+  },
+  {
+    "revision": "ab9a7a82bae0fc932dee",
+    "url": "/static/js/4.43a9abde.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/4.43a9abde.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7b38bbb15872bd2101fb",
+    "url": "/static/js/5.9dd34def.chunk.js"
+  },
+  {
+    "revision": "906d1ca04f2da983504d",
+    "url": "/static/js/6.cff02143.chunk.js"
+  },
+  {
+    "revision": "8fa5376b1e578af9d00e",
+    "url": "/static/js/7.ae3ed05e.chunk.js"
+  },
+  {
+    "revision": "05d99fec4472a6ca62a9",
+    "url": "/static/js/8.73b14e52.chunk.js"
+  },
+  {
+    "revision": "6ac45f744e535701d3d1",
+    "url": "/static/js/9.9379657e.chunk.js"
+  },
+  {
+    "revision": "73ceaa80faeaf10e1b1b",
+    "url": "/static/js/main.21fa4052.chunk.js"
+  },
+  {
+    "revision": "de6761babcb0bd4797b1",
+    "url": "/static/js/runtime-main.5f1be776.js"
+  },
+  {
+    "revision": "d53101c6ba249e306f3a59ed3e345c4f",
+    "url": "/static/media/me.d53101c6.png"
+  },
+  {
+    "revision": "cafef7769834bf5396d67f91efcb0540",
+    "url": "/static/media/test.cafef776.jpg"
+  }
+]);
